d3a7d0286082df8ccc839f49efb0100d08e079c4
Mabedin00 <mabedin00@stuy.edu>
2019-09-10 11:47:01 -0400

    finished

--- Rand Names/randomNames.py
+++ Rand Names/randomNames.py
@@ -1,3 +1,17 @@
+import random
 def randomNames(dict):
-    firstNum = random.randint(0,len(dict))
-    
+    teamNum = random.randint(0,len(dict)-1)
+    if (teamNum == 0):
+        tempList = dict["orpheus"]
+    elif (teamNum == 1):
+        tempList = dict["rex"]
+    else:
+        tempList = dict["endymion"]
+
+    nameNum = random.randint(0,len(tempList)-1)
+    print(tempList[nameNum])
+
+
+randomNames({"orpheus": ["Emily", "Kevin", "Vishwaa", "Eric"],
+             "rex":     ["William", "Joseph", "Calvin", "Ethan"],
+             "endymion":["Grace", "Nahi", "Derek", "Connor"]})
