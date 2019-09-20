1ae2289eeadf5bcb76b8ed4a57dd80c604b319d8
Mabedin00 <mabedin00@stuy.edu>
2019-09-20 09:18:33 -0400

    flask update

--- pairProgram/app.py
+++ pairProgram/app.py
@@ -1,8 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__) #create new instance of flask
 @app.route("/")
 def hello_world():
     return "No"
+
+coll = [0, 1, 1, 2, 3, 5, 8]
+
+@app.route("/my_foist_template")
+def foist():
+    return render_template("my_foist_template.html",
+                           collection = coll)
+
 
 if __name__ == "__main__":
     app.debug = True
