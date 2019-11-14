220a83e4ad21180b51fd5ffe07f0236fb9c0ef2d
Mohidul Abedin <mabedin00@stuy.edu>
2019-11-13 19:49:15 -0500

    added 24 to github

--- 24_rest/app.py
+++ 24_rest/app.py
@@ -5,13 +5,10 @@ import random
 app = Flask(__name__) #create new instance of flask
 @app.route("/")
 def root():
-    u = urllib.request.urlopen("https://restcountries.eu/rest/v2/name/usa")
-    #
+    u = urllib.request.urlopen("https://api.nasa.gov/planetary/apod?api_key=VdYsyzgDaCPGB3NbGK23gDxBonpCxadx0lroq37G")
     response = u.read()
     data = json.loads(response)
-    return render_template("index.html", name = response["name"])
-#
-# @app.route("/")
+    return render_template("index.html", pic = data["url"], desc = data["explanation"])
 
 if __name__ == "__main__":
     app.debug = True
