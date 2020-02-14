3dbaaa4cd3d6034d2a5eeef0e59adcf0c1449de4
Mohidul Abedin <mabedin00@stuy.edu>
2020-02-13 20:45:10 -0500

    done

--- 08_canvas-anim/animated.js
+++ 08_canvas-anim/animated.js
@@ -1,53 +1,28 @@
 var canvas = document.getElementById("playground");
 var ctx = canvas.getContext("2d");
-var anim = document.getElementById("animate");
-var stop = document.getElementById("stop");
-var dvd_btn = document.getElementById("corner")
-var logo = new Image();
 
+var circle_btn = document.getElementById("animate");
+var dvd_btn = document.getElementById("corner");
+var stop = document.getElementById("stop");
+
+var logo = new Image();
 logo.src = "logo_dvd.jpg"
 
-
-
 var animation_id;
-var logo_id;
-var dx;
-var dy;
+var x, y;
+var dx = 1;
+var dy = 1;
 var is_running = false;
 var is_increasing = true;
 var radius = 0;
 
-var start_animate = function(e) {
+var start_circle = function(e) {
 	e.preventDefault();
-	if (!is_running) {
-		is_running = true;
-		animation_id = window.requestAnimationFrame(animate);
-	}
-};
-
-var stop_animate = function(e) {
 	window.cancelAnimationFrame(animation_id);
-	is_running = false;
+	animation_id = window.requestAnimationFrame(animate_circle);
 };
 
-
-var logo_popup = function(e){ // initial popup of the logo AS THE NAME IMPLIES
-	console.log(e)
-	dx = Math.random()*canvas.width;
-	dy = Math.random()*canvas.height;
-	ctx.clearRect(0, 0, canvas.width, canvas.height);
-	ctx.drawImage(logo, dx, dy, 50, 50);
-	logo_id = window.requestAnimationFrame(logo_animate);
-
-};
-
-var logo_animate = function(e){
-
-
-};
-
-
-var animate = function(e) {
+var animate_circle = function(e) {
 	if (is_increasing) radius++;
 	else               radius--;
 
@@ -60,10 +35,40 @@ var animate = function(e) {
 	ctx.fill();
 	ctx.closePath();
 
-	if (is_running)
-		window.requestAnimationFrame(animate);
+	animation_id = window.requestAnimationFrame(animate_circle);
 }
 
-anim.addEventListener("click", start_animate);
-dvd_btn.addEventListener("click", logo_popup)
-stop.addEventListener("click", stop_animate);
+var start_logo = function(e) {
+	e.preventDefault();
+
+	x = 100 + Math.random() * (canvas.width  - 200);
+	y = 100 + Math.random() * (canvas.height - 200);
+
+	window.cancelAnimationFrame(animation_id);
+	animation_id = window.requestAnimationFrame(animate_logo);
+};
+
+var animate_logo = function(e) {
+	ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+	x += dx;
+	y += dy;
+
+	if (x < 0 || x > canvas.width  - 50) dx = -dx;
+	if (y < 0 || y > canvas.height - 50) dy = -dy;
+
+
+	ctx.drawImage(logo, x, y, 50, 50);
+	animation_id = window.requestAnimationFrame(animate_logo);
+};
+
+var stop_animation = function(e) {
+	window.cancelAnimationFrame(animation_id);
+	is_running = false;
+};
+
+
+
+circle_btn.addEventListener("click", start_circle);
+dvd_btn.addEventListener("click", start_logo);
+stop.addEventListener("click", stop_animation);
