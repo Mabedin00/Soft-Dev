f337f4a38a1ebcef2f22a18181189fcf8a557dd8
Mohidul Abedin <mabedin00@stuy.edu>
2020-02-27 22:31:30 -0500

    finished

--- 09_mongo/main.py
+++ 09_mongo/main.py
@@ -1,15 +1,39 @@
 from pymongo import MongoClient
-from pprint import pprint
-from bson.json_util import loads
+import pprint
 import json
-import datetime
 
-client = MongoClient()
-db = client.test
+
+client = MongoClient('localhost', 27017)
+db = client.primer
 restaurants = db.restaurants
-restaurants.delete_many({})
 
-with open("./primer-dataset.json", 'r') as file:
-    data = json.load(file)
-    for member in data:
-        id = restaurants.insert_one(loads(json.dumps(member)))
+def rest_by_bor(borough):
+    return list(restaurants.find({"borough": borough}))[:3]
+
+print("---------- 3 restaurants in a specified borough ----------")
+pprint.pprint(rest_by_bor("Staten Island"))
+
+def rest_by_zip(zip):
+    return list(restaurants.find({"address.zipcode": zip}))[:3]
+
+print("---------- 3 restaurants in a specified zip code. ----------")
+pprint.pprint(rest_by_zip("11228"))
+
+def rest_by_zip_grade(zip, grade):
+    return list(restaurants.find({"$and": [{"address.zipcode": zip}, {"grades.grade": grade} ]})) [:3]
+
+print("---------- restaurants in a specified zip code and with a specified grade ----------")
+
+pprint.pprint(rest_by_zip_grade("11228", "C"))
+
+def rest_by_zip_grade_1(zip, score):
+    return list(restaurants.find({"$and": [{"address.zipcode": zip}, {"grades.score": {"$lt": score}} ]})) [:3]
+print("---------- 3 restaurants in a specified zip code with a score below a specified threshold ----------")
+pprint.pprint(rest_by_zip_grade_1("10303", 6))
+
+def something_more_clever(zip, cuisine):
+    return list(restaurants.find({"$and": [{"address.zipcode": zip}, {"cuisine": cuisine} ]})) [:3]
+
+print("---------- Something more clever ----------")
+
+pprint.pprint(something_more_clever("10303", "Spanish"))
